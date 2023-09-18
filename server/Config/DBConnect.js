4888a34272897f96a046b2b4fac01868cb5f708d
hafizswabah <swabah.a.avd121@gmail.com>
2023-09-18 14:00:28 +0530

    project make ready to host

--- server/Config/DBConnect.js
+++ server/Config/DBConnect.js
@@ -1,6 +1,6 @@
 import Mongoose from 'mongoose'
 function DBConnect(){
-Mongoose.connect("mongodb://localhost:27017/expenseTracker")
+Mongoose.connect("mongodb+srv://swabahaavd121:2ES93iAWAAcn94CB@cluster0.ykczcfn.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{console.log('DB Connected');})
 .catch((err)=>{console.log('db error :',err);})
 }
